aa7d8618ae716a4ae93392becc2c614a9222f766
Maksim Shpontak <maximalla1997@gmail.com>
2023-09-13 19:41:56 +0300

    deleted unused files

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Scroll } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  // sr = ScrollReveal({});
 }
