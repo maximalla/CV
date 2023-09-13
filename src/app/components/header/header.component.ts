f14ceb6ed5c46f3513f005a148689b4336542644
Maksim Shpontak <maximalla1997@gmail.com>
2023-09-13 19:10:34 +0300

    updated the styles the unnecessary ones

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  toggleNavMenu() {
+    const navMenu = document.getElementById('nav-menu') as HTMLElement;
+    navMenu.classList.toggle('show');
+  }
+  hideNavMenu() {
+    const navMenu = document.getElementById('nav-menu') as HTMLElement;
+    navMenu.classList.remove('show');
+  }
 }
