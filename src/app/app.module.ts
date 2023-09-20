b4c42d35f388d84057799dcf24b1c8c444e20089
Maksim Shpontak <maximalla1997@gmail.com>
2023-09-20 16:00:52 +0300

    delete unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import { WorkComponent } from './components/work/work.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { EducationComponent } from './components/education/education.component';
@@ -20,7 +19,6 @@ import { ProjectsComponent } from './components/projects/projects.component';
     HomeComponent,
     AboutComponent,
     SkillsComponent,
-    WorkComponent,
     ContactComponent,
     FooterComponent,
     EducationComponent,
