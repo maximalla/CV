c1bbc5c33e4860fdbc289e36541db9b1e00fcfb4
Maksim Shpontak <maximalla1997@gmail.com>
2023-09-20 16:14:34 +0300

    Moved CV to "pages" folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { EducationComponent } from './components/education/education.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { HeaderComponent } from './pages/cv/components/header/header.component';
+import { HomeComponent } from './pages/cv/components/home/home.component';
+import { AboutComponent } from './pages/cv/components/about/about.component';
+import { SkillsComponent } from './pages/cv/components/skills/skills.component';
+import { ContactComponent } from './pages/cv/components/contact/contact.component';
+import { FooterComponent } from './pages/cv/components/footer/footer.component';
+import { EducationComponent } from './pages/cv/components/education/education.component';
+import { ExperienceComponent } from './pages/cv/components/experience/experience.component';
+import { ProjectsComponent } from './pages/cv/components/projects/projects.component';
+import { CVComponent } from './pages/cv/cv.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
     EducationComponent,
     ExperienceComponent,
     ProjectsComponent,
+    CVComponent,
   ],
   imports: [BrowserModule],
   providers: [],
