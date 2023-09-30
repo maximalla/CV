feaae335d891b14205ede2797af365e99d897374
Maksim Shpontak <maximalla1997@gmail.com>
2023-10-01 01:19:53 +0300

    add trailer in popup

--- src/app/pages/my-projects/pages/todo/services/todo.service.ts
+++ src/app/pages/my-projects/pages/todo/services/todo.service.ts
@@ -3,7 +3,6 @@ import { MessageService } from 'primeng/api';
 
 import { Todo } from '../shared/interfaces/todo';
 import { GeneralModel } from '../shared/types/general.model';
-import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,115 +10,78 @@ import { ApiService } from './api.service';
 export class TodoService {
   m!: GeneralModel;
 
-  constructor(
-    private readonly apiService: ApiService,
-    private readonly messageService: MessageService,
-  ) {}
+  constructor(private readonly messageService: MessageService) {}
 
   init(m: GeneralModel): void {
     this.m = m;
-    // this.loading = true;
-    this.m.subscriptions.push(
-      this.apiService.getTodos().subscribe({
-        next: (todos) => {
-          this.m.todos = todos;
-        },
-        error: (error) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Status Code: ' + JSON.stringify(error.status),
-            life: 3000,
-          });
-        },
-        complete: () => {
-          /* this.loading = false;*/
-        },
-      }),
-    );
+    const storedTodos: string | null = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.m.todos = JSON.parse(storedTodos);
+    }
   }
 
   createTodo(todo: Todo): void {
-    this.m.subscriptions.push(
-      this.apiService.addTodo(todo).subscribe({
-        next: (todo) => {
-          todo.id = Math.floor(Math.random() * 8000) + 201;
-          this.m.todos.unshift(todo);
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Successful',
-            detail: 'Task Created',
-            life: 3000,
-          });
-        },
-        error: (error) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Status Code: ' + JSON.stringify(error.status),
-            life: 3000,
-          });
-        },
-      }),
-    );
-  }
+    todo.id = Math.floor(Math.random() * 8000) + 201;
 
-  editTodo(todo: Todo) {
-    this.m.subscriptions.push(
-      this.apiService.updateTodo(todo).subscribe({
-        next: (updatedTodo) => {
-          const foundTodo = this.m.todos.find((t) => t.id === updatedTodo.id);
-          foundTodo
-            ? Object.assign(foundTodo, updatedTodo)
-            : this.m.todos.push(updatedTodo);
+    this.m.todos.unshift(todo);
 
-          delete this.m.editedTodos[todo.id as number];
+    this.saveTodosLocally();
 
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Successful',
-            detail: 'Task Updated',
-            life: 3000,
-          });
-        },
-        error: (error) => {
-          const index = this.m.todos.findIndex((t) => t.id === todo.id);
-          this.m.todos[index] = this.m.editedTodos[todo.id as number];
-          delete this.m.editedTodos[todo.id as number];
-          this.messageService.add({
-            severity: 'error',
-            summary: JSON.stringify(error.name).slice(1, -1),
-            detail: 'Status Code: ' + JSON.stringify(error.status),
-            life: 3000,
-          });
-        },
-      }),
-    );
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Successful',
+      detail: 'Task Created',
+      life: 3000,
+    });
   }
 
   deleteTodo(todo: Todo): void {
-    if (todo.id) {
-      this.m.subscriptions.push(
-        this.apiService.deleteTodo(todo.id).subscribe({
-          next: () => {
-            this.m.todos = this.m.todos.filter((val) => val.id !== todo.id);
-            this.messageService.add({
-              severity: 'success',
-              summary: 'Successful',
-              detail: 'Task Deleted',
-              life: 3000,
-            });
-          },
-          error: (error) => {
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Error',
-              detail: 'Status Code: ' + JSON.stringify(error.status),
-              life: 3000,
-            });
-          },
-        }),
-      );
+    const index: number = this.m.todos.findIndex((t) => t.id === todo.id);
+    if (index !== -1) {
+      this.m.todos.splice(index, 1);
+
+      this.saveTodosLocally();
+
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'Task Deleted',
+        life: 3000,
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Todo not found',
+        life: 3000,
+      });
     }
+  }
+
+  editTodo(todo: Todo): void {
+    const foundTodo = this.m.todos.find((t) => t.id === todo.id);
+    if (foundTodo) {
+      Object.assign(foundTodo, todo);
+
+      this.saveTodosLocally();
+
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'Task Updated',
+        life: 3000,
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Todo not found',
+        life: 3000,
+      });
+    }
+  }
+
+  private saveTodosLocally(): void {
+    localStorage.setItem('todos', JSON.stringify(this.m.todos));
   }
 }
